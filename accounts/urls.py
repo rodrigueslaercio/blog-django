a353c0064e14d60e0ec8e31b0de2549ca39f912f
rodrigueslaercio <laercio.rodrigs@gmail.com>
2024-01-17 16:58:20 -0300

    add user registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 """URL patterns for accounts"""
 
 from django.urls import path, include
+from . import views
 
 app_name = 'accounts'
 urlpatterns = [
     # Default auth urls
     path('', include('django.contrib.auth.urls')),
+    # Register page
+    path('register', views.register, name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login 
+from .forms import UserCreationForm
 
-# Create your views here.
+def register(request):
+    """Register a new user"""
+    if request.method != 'POST':
+        # Displays a blank form
+        form = UserCreationForm()
+    else:
+        # Process a completed form via POST
+        form = UserCreationForm(data=request.POST)
+        
+        if form.is_valid():
+            new_user = form.save()
+            
+            # Login and redirect
+            login(request, new_user)
+            return redirect('blog:index')
+    
+    context = {'form': form}
+    return render(request, 'registration/register.xhtml', context)
+    
+
